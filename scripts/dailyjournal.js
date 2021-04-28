514b0d9c86614d5bb02edcbbeb264072ac06a4d1
peytonkemp <peyton.kemp@gmail.com>
2021-04-28 16:31:19 -0400

    invoked JournalForm in DailyJournal function

--- scripts/dailyjournal.js
+++ scripts/dailyjournal.js
@@ -1,9 +1,14 @@
 import { Entries } from "./entries.js"
+import { JournalForm } from "./JournalForm.js"
 
 export const DailyJournal = () => {
     return `
         <div class="entryList">
             ${ Entries() }
         </div>
+
+        <article class="entryForm">
+            ${JournalForm()}
+        </article>
     `
 }
